6f8fba3283c5bd72a048f0284628a0af70361497
Roger Jönsson <rogerjoensson@outlook.com>
2016-12-13 14:04:17 +0100

    Add project files.

---
+++ DecorationSpecificationTemplateVSTO/ThisDocument.vb
@@ -0,0 +1,11 @@
+﻿Public Class ThisDocument
+
+    Private Sub ThisDocument_Startup() Handles Me.Startup
+
+    End Sub
+
+    Private Sub ThisDocument_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+End Class
